9a2da1c4a7099605a61a3a1fae5e67af2c1415f3
Stefanescu Cristian <howerkraft@gmail.com>
2012-12-17 16:37:05 +0200

    Added posibility to run an init.d script to chmod /dev/exynos-mem
    Just wanted to test my Java learing skills.

--- src/com/rc/exynosmemfix/MainActivity.java
+++ src/com/rc/exynosmemfix/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	TextView textVuln;
+	TextView textBoot;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +22,19 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		textVuln = (TextView) findViewById(R.id.textVuln);
+		textBoot = (TextView) findViewById(R.id.textBoot);
 		Button buttonFix = (Button) findViewById(R.id.buttonFix);
 		Button buttonUnFix = (Button) findViewById(R.id.buttonUnFix);
+		Button buttonSetOnBoot = (Button) findViewById(R.id.buttonSetOnBoot);
+		Button buttonUnSetOnBoot = (Button) findViewById(R.id.buttonUnSetOnBoot);
 
 		buttonFix.setOnClickListener(fixClickListener);
 		buttonUnFix.setOnClickListener(unFixClickListener);
+		buttonSetOnBoot.setOnClickListener(setOnBoot);
+		buttonUnSetOnBoot.setOnClickListener(unSetOnBoot);
 
 		checkIfVuln();
-	}
+		checkIfBoot();}
 
 	private void checkIfVuln() {
 		try {
@@ -36,21 +42,40 @@ public class MainActivity extends Activity {
 			if (r.stdout.contains("rw-rw-rw-")) {
 				textVuln.setText("/dev/exynos-mem is vulnerable!");
 				textVuln.setTextColor(Color.RED);
-			} else {
+			} 
+			else {
 				textVuln.setText("/dev/exynos-mem is NOT vulnerable!");
 				textVuln.setTextColor(Color.GREEN);
 			}
-		} catch (Exception ex) {
+		}
+		catch (Exception ex) {
 			textVuln.setText("Unable to determine vulnerability status - is your device rooted?");
 			textVuln.setTextColor(Color.YELLOW);
 		}
 	}
-
+	private void checkIfBoot() {
+		try {
+			VTCommandResult rb = VirtualTerminal.run("ls -l /etc/init.d/", true);
+			if (rb.stdout.contains("70-exynosfix.sh")) {
+				textBoot.setText("Patch is applied on boot!");
+				textBoot.setTextColor(Color.GREEN);
+			}
+			else {
+				textBoot.setText("Patch is not applied on boot!");
+				textBoot.setTextColor(Color.RED);
+			}
+		}
+		catch (Exception ex) {
+			textBoot.setText("Unable to determine vulnerability status - is your device rooted?");
+			textBoot.setTextColor(Color.YELLOW);
+		}
+	}
+		
 	OnClickListener fixClickListener = new OnClickListener() {
 
 		@Override
 		public void onClick(View v) {
-			VTCommandResult r = VirtualTerminal.run("chmod 600 /dev/exynos-mem", true);
+			VTCommandResult r = VirtualTerminal.run("chmod 400 /dev/exynos-mem", true);
 			if (!r.success()) {
 				Toast.makeText(MainActivity.this, "Error: " + r.stderr, Toast.LENGTH_LONG).show();
 			}
@@ -69,5 +94,29 @@ public class MainActivity extends Activity {
 			checkIfVuln();
 		}
 	};
+	
+	OnClickListener setOnBoot = new OnClickListener() {
 
+		@Override
+		public void onClick(View v) {
+			VTCommandResult rb = VirtualTerminal.run("mount -o remount,rw /system; touch /etc/init.d/70-exynosfix.sh; echo 'chmod 400 /dev/exynos-mem' > /etc/init.d/70-exynosfix.sh; mount -o remount,ro /system", true);
+			if (!rb.success()) {
+                                Toast.makeText(MainActivity.this, "Error: " + rb.stderr, Toast.LENGTH_LONG).show();
+                        }
+                        checkIfBoot();
+                 }
+	};
+	
+	OnClickListener unSetOnBoot = new OnClickListener() {
+	
+		@Override
+		public void onClick(View v) {
+			VTCommandResult rb = VirtualTerminal.run("mount -o remount,rw /system; rm /etc/init.d/70-exynosfix.sh; mount -o remount,ro /system", true);
+			if (!rb.success()) {
+			      Toast.makeText(MainActivity.this, "Error: " + rb.stderr, Toast.LENGTH_LONG).show();
+			}
+			checkIfBoot();
+	}
+
+	};
 }
